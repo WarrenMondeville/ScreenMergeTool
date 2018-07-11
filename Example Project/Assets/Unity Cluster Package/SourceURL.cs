df725cc0635b7e5209417223a598f6f4b1a29933
WarrenMondeville <541008505@qq.com>
2018-07-11 18:35:05 +0800

    addd project

---
+++ Example Project/Assets/Unity Cluster Package/SourceURL.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SourceURL : MonoBehaviour {
+
+    public string url = "https://sourceforge.net/projects/unityclusterpackage/?source=typ_redirect";
+}
